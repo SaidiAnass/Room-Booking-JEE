7df4d0230d640f763af88553d1c1b7c12c1941e3
Oussama Lamnaouer <lmnouer59@gmail.com>
2022-05-04 11:57:47 +0000

    set up room servlet

--- src/main/java/com/miola/roombooking/controllers/RoomServlet.java
+++ src/main/java/com/miola/roombooking/controllers/RoomServlet.java
@@ -6,15 +6,55 @@ import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 
-@WebServlet(name = "RoomServlet", value = "/room-servlet")
+@WebServlet(name = "RoomServlet", value = "/*.room")
 public class RoomServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        String Path = request.getServletPath();
+        //=========================================================================\\
+        if (Path.equalsIgnoreCase("/list.room")) {
+            // get rooms list
+            request.getRequestDispatcher("views/rooms-list.jsp").forward(request, response);
+        }
+        //=========================================================================\\
+        else if (Path.equalsIgnoreCase("/add.room")) {
+            request.getRequestDispatcher("views/add-room.jsp").forward(request, response);
 
+        }if (Path.equalsIgnoreCase("/save.room")) {
+            // get infos and save action (add,edit,delete) to database
+            request.getRequestDispatcher("views/rooms-list.jsp").forward(request, response);
+        }
+        //=========================================================================\\
+        else if (Path.equalsIgnoreCase("/edit.room")) {
+            //            Long userID = Long.valueOf(request.getParameter("id"));
+            // get id and go to edit room
+            request.getRequestDispatcher("views/edit-room.jsp").forward(request, response);
+        }
+        //=========================================================================\\
+        else if (Path.equalsIgnoreCase("/delete.room")) {
+
+            // get id and go to delete room
+            request.getRequestDispatcher("views/delete-room.jsp").forward(request, response);
+            //=========================================================================\\
+        }else if (Path.equalsIgnoreCase("/sign-in.room")) {
+            // get infos and authenticate
+            request.getRequestDispatcher("index.jsp").forward(request, response);
+            //=========================================================================\\
+        }else if (Path.equalsIgnoreCase("/register.room")) {
+            // get infos and create account
+            request.getRequestDispatcher("index.jsp").forward(request, response);
+            //=========================================================================\\
+        }else if (Path.equalsIgnoreCase("/index.room")) {
+            request.getRequestDispatcher("index.jsp").forward(request, response);
+            //=========================================================================\\
+        }else{
+            request.getRequestDispatcher("404.jsp").forward(request, response);
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request,response);
     }
 }
