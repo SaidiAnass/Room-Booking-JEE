58fa8154e96f613dcfb90f3d0408be69c00bfa99
Oussama Lamnaouer <lmnouer59@gmail.com>
2022-05-04 11:57:32 +0000

    set up admin servlet

--- src/main/java/com/miola/roombooking/controllers/AdminServlet.java
+++ src/main/java/com/miola/roombooking/controllers/AdminServlet.java
@@ -10,7 +10,37 @@ import java.io.IOException;
 public class AdminServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        String Path = request.getServletPath();
 
+
+        //========================== Admin's Actions ============================\\
+        if (Path.equalsIgnoreCase("/list.admin")) {
+            // get admins list
+            request.getRequestDispatcher("views/admins-list.jsp").forward(request, response);
+        }
+        //=========================================================================\\
+        else if (Path.equalsIgnoreCase("/add.admin")) {
+            request.getRequestDispatcher("views/add-admin.jsp").forward(request, response);
+
+        }if (Path.equalsIgnoreCase("/save.admin")) {
+            // get infos and save action (add,edit,delete) to database
+            request.getRequestDispatcher("views/admins-list.jsp").forward(request, response);
+        }
+        //=========================================================================\\
+        else if (Path.equalsIgnoreCase("/edit.admin")) {
+            //            Long userID = Long.valueOf(request.getParameter("id"));
+            // get id and go to edit admin
+            request.getRequestDispatcher("views/edit-admin.jsp").forward(request, response);
+        }
+        //=========================================================================\\
+        else if (Path.equalsIgnoreCase("/delete.admin")) {
+
+
+            // get id and go to delete admin
+            request.getRequestDispatcher("views/delete-admin.jsp").forward(request, response);
+
+        }
     }
 
     @Override
