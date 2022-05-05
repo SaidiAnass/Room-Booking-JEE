f6326dc75ef2bd5709aa8d1b9d253868e48dbf52
Oussama Lamnaouer <lmnouer59@gmail.com>
2022-05-05 23:46:30 +0000

    admin crud operations

--- src/main/java/com/miola/roombooking/dao/AdminDao.java
+++ src/main/java/com/miola/roombooking/dao/AdminDao.java
@@ -40,8 +40,82 @@ public class AdminDao {
         }
         return adminsList;
     }
-    public void getAdminById(){}
-    public void addAdmin(Admin ad){}
-    public void deleteAdmin(Admin ad){}
-    public void updateAdmin(Admin ad){}
+
+
+    public Admin getAdminByEmail(String email){
+        String query = "SELECT * FROM admin WHERE email like '" + email +"'";
+        Statement stmt = null;
+        try {
+            stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while(rs.next()) {
+                return new Admin(rs.getInt("adminId"), rs.getString("firstName"), rs.getString("lastName"), rs.getString("email"), rs.getString("password"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+    public Admin getAdminById(int id){
+        String query = "SELECT * FROM admin WHERE adminId like '" + id +"'";
+        Statement stmt = null;
+        try {
+            stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            System.out.printf("Here");
+            while(rs.next()) {
+                return new Admin(rs.getInt("adminId"), rs.getString("firstName"), rs.getString("lastName"), rs.getString("email"), rs.getString("password"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.printf("Admin not  found");
+        return null;
+    }
+
+    /* Add Admin */
+    public boolean addAdmin(Admin ad){
+        if(getAdminByEmail(ad.getEmail()) != null){
+            System.out.println("Admin existe");
+            return false;
+        }
+        if(ad.getEmail() == null || ad.getPassword() == null){
+            return false;
+        }
+        String query = "INSERT INTO admin (firstName,lastName,email,password) VALUES" +
+                "('"+ad.getFirstName()+"','"+ad.getLastName()+"','"+ad.getEmail()+"','"+ad.getPassword()+"')";
+        Statement stmt = null;
+        int rs=0;
+        try {
+            stmt = con.createStatement();
+            rs = stmt.executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return rs > 0;
+    }
+
+
+
+
+    public boolean deleteAdmin(Admin ad){
+        String query = "DELETE FROM admin WHERE adminId like '"+ad.getAdminId()+"'";
+        Statement stmt = null;
+        int rs=0;
+        try {
+            stmt = con.createStatement();
+            rs = stmt.executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return rs > 0;
+    }
+
+
+    public void updateAdmin(Admin ad){
+
+    }
 }
--- src/main/java/com/miola/roombooking/models/Admin.java
+++ src/main/java/com/miola/roombooking/models/Admin.java
@@ -65,4 +65,5 @@ public class Admin {
     public void setPassword(String password) {
         this.password = password;
     }
+
 }
