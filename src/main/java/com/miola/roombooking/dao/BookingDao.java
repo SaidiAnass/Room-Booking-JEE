931cc016b156337dc4d7aaf89f99b645ea089d9e
Oussama Lamnaouer <lmnouer59@gmail.com>
2022-05-07 01:35:00 +0000

    add booking to database

--- src/main/java/com/miola/roombooking/dao/BookingDao.java
+++ src/main/java/com/miola/roombooking/dao/BookingDao.java
@@ -5,14 +5,42 @@ import com.miola.roombooking.models.Booking;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class BookingDao {
     private Connection con;
 
-    public BookingDao() throws SQLException {
-        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/room-booking","root","");
+    public BookingDao(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/room-booking","root","");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
+    public boolean addBooking(Booking booking){
+//        if(getClientByEmail(cli.getEmail()) != null){
+//            System.out.println("CLient existe");
+//            return false;
+//        }
+//        if(cli.getEmail() == null || cli.getPassword() == null){
+//            return false;
+//        }
+        String query = "INSERT INTO booking (clientId,roomId,numberOfNights,startDate,endDate,price) VALUES" +
+                "('"+booking.getClientId()+"','"+booking.getRoomId()+"','"+booking.getNumberONights()+"','"+booking.getStartDate()+"','"+booking.getEndDate()+"','"+booking.getPrice()+"')";
+        Statement stmt = null;
+        int rs=0;
+        try {
+            stmt = con.createStatement();
+            rs = stmt.executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
+        return rs > 0;
+    }
     public Booking getBookingById(){
         return  null;
     }
--- src/main/java/com/miola/roombooking/models/Booking.java
+++ src/main/java/com/miola/roombooking/models/Booking.java
@@ -6,9 +6,86 @@ public class Booking {
     private int bookingId;
     private int clientId;
     private int roomId;
-    private Date startDate;
-    private Date endDate;
+    private String startDate;
+    private String endDate;
     private int numberONights;
+    private float price;
 
+    public Booking() {
+    }
 
+    public Booking(int bookingId, int clientId, int roomId, String startDate, String endDate, int numberONights, float price) {
+        this.bookingId = bookingId;
+        this.clientId = clientId;
+        this.roomId = roomId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.numberONights = numberONights;
+        this.price = price;
+    }
+
+    public Booking(int clientId, int roomId, String startDate, String endDate, int numberONights, float price) {
+        this.clientId = clientId;
+        this.roomId = roomId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.numberONights = numberONights;
+        this.price = price;
+    }
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public int getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(int clientId) {
+        this.clientId = clientId;
+    }
+
+    public int getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public int getNumberONights() {
+        return numberONights;
+    }
+
+    public void setNumberONights(int numberONights) {
+        this.numberONights = numberONights;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
