07dd8b388c0130c260d1475fa0eb4f0586a0933f
Oussama Lamnaouer <lmnouer59@gmail.com>
2022-05-08 03:26:52 +0100

    update booking dao

--- src/main/java/com/miola/roombooking/dao/BookingDao.java
+++ src/main/java/com/miola/roombooking/dao/BookingDao.java
@@ -1,7 +1,6 @@
 package com.miola.roombooking.dao;
 
 import com.miola.roombooking.models.Booking;
-import com.miola.roombooking.models.Client;
 
 import java.sql.*;
 import java.util.LinkedList;
@@ -19,6 +18,8 @@ public class BookingDao {
             e.printStackTrace();
         }
     }
+
+    /* Add Booking */
     public boolean addBooking(Booking booking){
         /* See if the room is Availabale */
         // get all the bookings for this room
@@ -61,9 +62,31 @@ public class BookingDao {
 
         return rs > 0;
     }
-    public LinkedList<Booking> getBookingsByRoomId(int id){
+
+    /* All Bookings */
+    public LinkedList<Booking> getAllBookings(){
         LinkedList<Booking> bookings= new LinkedList<>();
 
+
+        Statement stmt = null;
+        try {
+            stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from booking");
+
+            while (rs.next()) {
+                Booking booking= new Booking(rs.getInt("bookingId"),rs.getInt("clientId"), rs.getInt("roomId"), rs.getString("startDate"),rs.getString("endDate"),rs.getInt("numberOfNights"),rs.getFloat("price")) ;
+                bookings.add(booking);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return bookings;
+    }
+
+    /* RoomID => All Bookings with this ID */
+    public LinkedList<Booking> getBookingsByRoomId(int id){
+        LinkedList<Booking> bookings= new LinkedList<>();
 
         Statement stmt = null;
         try {
@@ -82,10 +105,86 @@ public class BookingDao {
         return bookings;
 
     }
-    public Booking getBookingByCLientId(){
-        return  null;
+
+    /* ClientID => All Bookings with this ID */
+    public LinkedList<Booking> getBookingByCLientId(int id){
+        LinkedList<Booking> bookings= new LinkedList<>();
+
+
+        Statement stmt = null;
+        try {
+            stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from booking WHERE clientId = '"+id+"'");
+
+
+            while (rs.next()) {
+                Booking booking= new Booking(rs.getInt("bookingId"),rs.getInt("clientId"), rs.getInt("roomId"), rs.getString("startDate"),rs.getString("endDate"),rs.getInt("numberOfNights"),rs.getFloat("price")) ;
+                bookings.add(booking);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return bookings;
     }
-    public Booking getBookingByRoomId(){
-        return  null;
+
+    /* Delete Booking */
+    public boolean deleteBooking(Booking booking){
+        String query = "DELETE FROM booking WHERE bookingId like '"+booking.getBookingId()+"'";
+        Statement stmt = null;
+        int rs=0;
+        try {
+            stmt = con.createStatement();
+            rs = stmt.executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return rs > 0;
+    }
+
+    /* Update Booking */
+    public boolean updateBooking(Booking booking){
+        /* See if the room is Availabale */
+        // get all the bookings for this room
+        LinkedList<Booking> bookings = getBookingsByRoomId(booking.getRoomId());
+        int newBookingStartMonth= Integer.parseInt(booking.getStartDate().split("/")[0]);
+        int newBookingStartDay= Integer.parseInt(booking.getStartDate().split("/")[1]);
+        int newBookingEndMonth= Integer.parseInt(booking.getEndDate().split("/")[0]);
+        int newBookingEndDay= Integer.parseInt(booking.getEndDate().split("/")[1]);
+
+        for(Booking bk: bookings){
+            int startMonth= Integer.parseInt(bk.getStartDate().split("/")[0]);
+            int startDay= Integer.parseInt(bk.getStartDate().split("/")[1]);
+            int endMonth= Integer.parseInt(bk.getEndDate().split("/")[0]);
+            int endDay= Integer.parseInt(bk.getEndDate().split("/")[1]);
+            if(newBookingStartMonth == startMonth){
+                if(startDay < newBookingStartDay){
+                    if(endMonth == newBookingEndMonth){
+                        if(endDay > newBookingStartDay){
+                            return false;
+                        }
+                    }
+
+                }
+            }
+            if(newBookingStartMonth == endMonth){
+                if (newBookingStartDay<endDay) return false;
+            }
+        }
+
+
+
+        String query = "UPDATE booking SET clientId = '"+booking.getClientId()+"', roomId = '"+booking.getRoomId()+"', startDate = '"+booking.getStartDate()+"', endDate = '"+booking.getEndDate()+"', price = "+booking.getPrice()+", numberOfNights = "+booking.getNumberONights()+" WHERE bookingId LIKE "+booking.getBookingId();
+        System.out.println("Updatiing 2 ..");
+        int res=0;
+        try {
+            Statement stmt = con.createStatement();
+            res = stmt.executeUpdate(query);
+        } catch (SQLException e) {
+            System.out.println(e);
+            e.printStackTrace();
+        }
+        return res>0;
     }
 }
