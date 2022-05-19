5d79b88fadf5069e36ab06d9426e6032e44848d7
Oussama Lamnaouer <lmnouer59@gmail.com>
2022-05-20 00:06:38 +0100

    get bookings by client

---
+++ src/main/java/com/miola/roombooking/controllers/ProfileServlet.java
@@ -0,0 +1,64 @@
+package com.miola.roombooking.controllers;
+
+import com.miola.roombooking.dao.AdminDao;
+import com.miola.roombooking.dao.BookingDao;
+import com.miola.roombooking.models.Admin;
+import com.miola.roombooking.models.Booking;
+import com.miola.roombooking.models.Client;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.LinkedList;
+
+
+@MultipartConfig
+@WebServlet(name = "ProfileServlet", value = "/profile")
+
+public class ProfileServlet extends HttpServlet{
+    // This servlet is for crud operations on admins
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        String Path = request.getServletPath();
+        BookingDao bookingDao = new BookingDao();
+
+        // set default return page
+        String returnPath = "profile.jsp";
+
+
+        if (Path.equalsIgnoreCase("/profile")) {
+            // check if user is authenticated
+            Client client =(Client) request.getSession().getAttribute("loggedIn");
+
+            // if auth
+            if(client == null){
+                returnPath ="/main";
+            }
+            // if not
+            else{
+                // get all bookings
+                LinkedList<Booking> bookings = bookingDao.getBookingsByCLientId(client.getClientId());
+                boolean bookingsExist = bookings != null;
+                request.setAttribute("bookings" , bookings);
+                request.setAttribute("bookingsExist" , bookingsExist);
+
+            }
+
+
+
+            request.getRequestDispatcher(returnPath).forward(request, response);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+
+
+}
